4b76724bfd80d67d9bb8054e488053e5af983cd4
root <root@DESKTOP-598GE48.localdomain>
2023-10-07 20:22:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Define the reducer function
+function reducer(batteryArray) {
+  // Initialize totalBatteries variable
+  let totalBatteries = 0;
+
+  // Iterate through the batteryArray and calculate the sum
+  batteryArray.forEach(battery => {
+    totalBatteries += battery;
+  });
+
+  // Return the totalBatteries
+  return totalBatteries;
+}
+
+// Calculate the total number of batteries
+const totalBatteries = reducer(batteryBatches);
+
+// Log the result
+console.log(`Total number of batteries: ${totalBatteries}`);
